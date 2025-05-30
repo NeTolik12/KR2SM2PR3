4f30e10b742b803f07a198d91ccad5b7c57673b0
NeTolik12 <denisakimov902@gmail.com>
2025-05-30 13:27:21 +0300

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ export function Footer(){
     return (
         <footer className="bg-[var(--color-header-footer-bg)] text-black py-4 mt-10">
             <div className="container mx-auto px4 flex justify-between items-center text-sm">
-                <span>Sh1VeN_ © {new Date().getFullYear()}</span>
+                <span>TRACY © {new Date().getFullYear()}</span>
                 <a
-                    href="https://github.com/NeTolik12/KR2_SM2_PR3_V2"
+                    href="https://github.com/tracylick61/ReactRecips"
                     className=""
                 >
                 Repository
